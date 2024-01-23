d9853d2287985f3fa96ff539dd10c06d47853f0d
Anders Hansen <58811906+adidas420x@users.noreply.github.com>
2024-01-23 15:07:14 +0100

    added FoodItem and NutritionalTable classes

---
+++ CalorieTracker/Models/FoodItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class FoodItem
+    {
+        public string Name { get; set; } = "DEFAULT";
+        public string Brand { get; set; } = "DEFAULT";
+        public string ImageLink { get; set; } = "DEFAULT";
+        public double? Weight { get; set; } = null;
+        public double? Volume { get; set; } = null;
+        public bool Favorite { get; set; } = false;
+        public string Ingredients { get; set; } = "DEFAULT";
+        public NutritionalTable NutritionalTable { get; set; } = new();
+    }
+}
---
+++ CalorieTracker/Models/NutritionalTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class NutritionalTable
+    {
+        public double EnergyKJ { get; set; } = -1;
+        public double EnergyKcal { get; set; } = -1;
+        public double Fat { get; set; } = -1;
+        public double FatSaturated { get; set; } = -1;
+        public double Carbohydrates { get; set; } = -1;
+        public double Sugars { get; set; } = -1;
+        public double DietaryFibers { get; set; } = -1;
+        public double Protein { get; set; } = -1;
+        public double Salt { get; set; } = -1;
+    }
+}
